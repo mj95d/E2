f28d9603a83171293ef4005a9b544eeba7188289
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-21 16:27:01 +0300

    ShcoolService.java

--- Exam2/src/test/java/Service/ShcoolService.java
+++ Exam2/src/test/java/Service/ShcoolService.java
@@ -1,88 +1,69 @@
 package Service;
 
-import Model.Studint;
-import Model.Teacher;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.test1.Model.Student;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 
 import java.util.ArrayList;
 
 @Service
-public class ShcoolService {
+public class StudentService {
 
+    ArrayList<Student> students = new ArrayList<>();
+    public ArrayList<Student> getAll(){
+        return students;
+    }
 
-        private ArrayList<Studint> students = new ArrayList<>();
-
-
-        private ArrayList<Teacher> teachers = new ArrayList<>();
-
-
-        public ArrayList<Studint> getAllStudents() {
-            return students;
-        }
-
-
-        public void addStudent(Studint student) {
-            students.add(student);
-        }
-
-
-        public void updateStudent(String id, Studint student) {
-            for (int i = 0; i < students.size(); i++) {
-                Studint s = students.get(i);
-                if (s.getId().equals(id)) {
-                    students.set(i, student);
-                    return;
-                }
-            }
-        }
-
-
-        public void deleteStudent(String id) {
-            students.removeIf(s -> s.getId().equals(id));
-        }
-
-
-        public ArrayList<Teacher> getAllTeachers() {
-            return teachers;
-        }
-
-
-        public void addTeacher(Teacher teacher) {
-            teachers.add(teacher);
-        }
-
-
-        public void updateTeacher(String id, Teacher teacher) {
-            for (int i = 0; i < teachers.size(); i++) {
-                Teacher t = teachers.get(i);
-                if (t.getId().equals(id)) {
-                    teachers.set(i, teacher);
-                    return;
-                }
-            }
-        }
-
-
-        public void deleteTeacher(String id) {
-            teachers.removeIf(t -> t.getId().equals(id));
-        }
-    public Teacher getTeacherById(String id) {
-        for (Teacher t : teachers) {
-            if (t.getId().equals(id)) {
-                return t;
-            }
+    public Student get(Student student){
+        for(int i = 0; i < students.size(); i++){
+            if(students.get(i).getName().equals(student.getName()))
+                return students.get(i);
         }
         return null;
     }
 
+    public void add(Student student){
+        students.add(student);
+    }
 
-    public Studint getStudentByName(String name) {
-        for (Studint s : students) {
-            if (s.getName().equals(name)) {
-                return s;
+    public Boolean update(int id, Student student){
+        for(int i = 0; i < students.size(); i++){
+            if(students.get(i).equals(id)) {
+                students.set(i, student);
+                return true;
             }
         }
+
+        return false;
+    }
+
+    public Boolean delete(int id){
+        for(int i = 0; i < students.size(); i++){
+            if(students.get(i).equals(id))
+                students.remove(i);
+                return true;
+        }
+        return false;
+    }
+
+    public ArrayList<String> getAllErrors(Errors errors){
+        ArrayList<String> allErrors =  new ArrayList<>();
+        if(errors.hasErrors()) {
+            for (int i = 0; i < errors.getAllErrors().size(); i++) {
+                String error = errors.getFieldErrors().get(i).getField().concat(" : " + errors.getAllErrors().get(i).getDefaultMessage());
+                allErrors.add(error);
+            }
+            return allErrors;
+        }
         return null;
+    }
+
+    public Boolean checkId(int id){
+        for(int i = 0; i < students.size(); i++){
+            if(Integer.parseInt(students.get(i).getId()) == id)
+                return true;
+        }
+
+        return false;
     }
 }
